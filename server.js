0b7029400f473f19bf0878ccf29d9bf645453ddc
amna-alsuwaidi <g00093153@aus.edu>
2025-03-01 14:46:50 +0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/roll", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
