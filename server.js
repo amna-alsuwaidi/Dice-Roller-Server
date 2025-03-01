c0f3435e2a943b0c90edd25559adad6f9883065e
amna-alsuwaidi <g00093153@aus.edu>
2025-03-01 15:57:17 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express()
 var cors = require('cors')
 const port = 8080
 
-app.get('/', (req, res) => {
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/', cors(corsOptions), function (req, res, next) {
   res.send('Hello World!')
 })
 
